2cd716306d74ce03292abf89a728cec1c0e43acb
khoinguyen09092k <nguyendangkhoi0909@gmail.com>
2022-08-29 17:17:42 +0700

    up date full

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomeShoeShop from "./Components/BTShoeShop/HomeShoeShop";
+function App() {
+  return (
+    <div className="App">
+   
+
+   <HomeShoeShop/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/BTShoeShop/RenderShoe.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class RenderShoe extends Component {
+  render() {
+    const {products,handleShoe,handleDetail,buyCarts,storeCounter,number1} = this.props 
+    return (
+        <div className='card'>
+        <img src={products.image} alt="" />
+
+
+        <div className='card-body'>
+            <h6>{products.name}</h6>
+            <p>{products.price}</p>
+            <button className="btn btn-dark" 
+             onClick={()=>{
+              buyCarts(products)
+              storeCounter(number1)
+            }}
+            >Add to carts <i className="fa-solid fa-cart-shopping" ></i></button>
+            
+            <button className='btn btn-warning ml-2' onClick={() => {
+            handleDetail()
+              handleShoe(products)
+
+            }} >Detail</button>
+        </div>
+    </div>
+    )
+  }
+}
